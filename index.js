0b3a0170adb6be1fc0cf0da54a438fdad85706ba
Ashish Agarwal <asagarw@microsoft.com>
2017-05-12 20:12:26 +0530

    added mongodb

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -25,7 +28,7 @@ app.use(bodyParser.urlencoded({
 //     });
 // });
 
-var cats = require('./cats.js')(app);
+var cats = require('./cat_routes.js')(app);
 
 var server = app.listen(3000, function () {
     console.log('Server running at http://127.0.0.1:3000/');
