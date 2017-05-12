bcdf686bec019c4b52347de30fa3cd64e069aab4
Ashish Agarwal <asagarw@microsoft.com>
2017-05-12 12:20:05 +0530

    initial

---
+++ index.js
@@ -0,0 +1,32 @@
+// var http = require('http');
+
+// http.createServer(function(req, res) {
+//     res.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+//     res.end('Hello World\n');
+// }).listen(3000, '127.0.0.1');
+
+// console.log('Server running at http://127.0.0.1:3000/');
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// app.get('/', function (req, res) {
+//     // res.send('Hello World!');
+//     res.json({
+//         hello: 'world'
+//     });
+// });
+
+var cats = require('./cats.js')(app);
+
+var server = app.listen(3000, function () {
+    console.log('Server running at http://127.0.0.1:3000/');
+})
